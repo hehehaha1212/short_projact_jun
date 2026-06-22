55097965072dec7eac956710ec4eedbf4689e04f
Aditi Rai <aditirai624@gmail.com>
2026-06-22 20:48:10 +0530

    carousel update

--- short_project_jun6/src/App.jsx
+++ short_project_jun6/src/App.jsx
@@ -5,11 +5,13 @@ import ContactUs from './pages/ContactUs'
 import FeaturesSection from './components/landingpage/Features'
 import ServiceSection from './components/landingpage/OurServices'
 import AboutSection from './components/landingpage/AboutSection'
+import HeroSection from './components/landingpage/Hero'
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <HeroSection/>
       <ServiceSection/>
       <FeaturesSection />
       <AboutSection/>
